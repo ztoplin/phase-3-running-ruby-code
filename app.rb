a4db40b3fe0dfe9a500157d7396dd962b82cd6d9
Aaron <ztoplin@yahoo.com>
2022-11-20 16:14:05 -0500

    commit tree fd7016996e8278c3ace4a5b98d797ff28d78b205

---
+++ app.rb
@@ -0,0 +1,33 @@
+# puts "Hello, world!"
+
+# # also can be written as, as using parentheses when invoking a method is optional:
+
+# puts("Hello, world!")
+
+# puts "Hello, world!"
+
+# # 'puts' adds a line break to the end of the string, whereas 'print' will not
+
+# print "Hello, world!"
+# print "Hello, world!"
+# print "Hello, world!"
+
+
+# puts [1, 2, 3]
+
+
+# # This is the equivalent of calling:
+# # puts [1, 2, 3].inspect 
+
+# p [1, 2, 3]
+
+
+# # pp, 'pretty-printing', is used for complex data such as nested arrays and hashes (the Ruby equivalent of JavaScript objects). it is calling the .pretty_inspect method
+
+# pp [{ id: 1, hello: "World"}, { id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+
+puts "Hello World!\n"
+
+print "Pass this test, please."
+
+p [1,2,3]
